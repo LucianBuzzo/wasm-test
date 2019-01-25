f473bc126847938c2997f9b3db91b5dcc9da71f5
Lucian <lucian.buzzo@gmail.com>
2019-01-25 12:54:46 +0000

    Get formulas running

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import * as cdsl from "balena-cdsl";
+import * as temen from "balena-temen";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Box, Provider } from "rendition";
-import { Form } from "rendition/dist/unstable";
+import { Box, Provider, Txt } from "rendition";
+import Form from "react-jsonschema-form";
+import * as _ from 'lodash'
 
 const balenaSchema = `
   title: pricing
@@ -11,22 +13,15 @@ const balenaSchema = `
     - devices:
         title: Devices
         type: number
+        default: 1000
     - monthsCommit:
         title: Commitment (months)
-        default: 0
+        default: 12
         type: number
     - monthsPrepay:
         title: Prepayment (months)
-        default: 0
+        default: 20
         type: number
-    - foo:
-        type: number
-        formula: uuidv4()
-        $$formula: uuidv4()
-
-    - bar:
-        type: number
-        eval: foo + 10
     - deviceClass:
         type: string?
         default: standard
@@ -42,11 +37,39 @@ const balenaSchema = `
           - value: fixed
     - microservices:
         type: boolean
+    - unit:
+        title: Base unit price
+        type: number
+        default: 1
+    - Total:
+        formula: >
+          devices * monthsCommit * unit
+        readOnly: true
+        type: number
 `
 
 const schema = cdsl.generate_ui(balenaSchema)
 
 console.log(schema)
+
+console.log(temen)
+
+var isobject = function(x){
+    return Object.prototype.toString.call(x) === '[object Object]';
+};
+
+const getFormulaKeys = function(obj, prefix){
+  var keys = Object.keys(obj);
+  prefix = prefix ? prefix + '.' : '';
+  return keys.reduce(function(result, key){
+    if(isobject(obj[key])){
+      result = result.concat(getFormulaKeys(obj[key], prefix + key));
+    }else if (key === '$$formula') {
+      result.push(prefix + key);
+    }
+    return result;
+  }, []);
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -59,27 +82,52 @@ class App extends React.Component {
 
   onChange = ({ formData }) => {
     console.log(formData);
-    this.setState({ formData });
+
+    console.log(getFormulaKeys(schema.json_schema))
+
+    const keys = getFormulaKeys(schema.json_schema).map(path => {
+      return {
+        formula: _.get(schema.json_schema, path),
+        path: path.replace(/properties\./g, '')
+      }
+    })
+
+    const data = _.cloneDeep(formData)
+
+    keys.forEach((y) => {
+      _.set(data, y.path, y.formula)
+    })
+
+    console.log( { keys })
+
+    console.log(data)
+
+    let evaluate
+
+    try {
+      evaluate = temen.evaluate(data)
+    } catch (e) {
+      console.error(e)
+      evaluate = formData
+    }
+
+    console.log(evaluate)
+
+    this.setState({ formData: _.merge({}, formData, evaluate) });
+
   };
 
   render() {
     const { formData } = this.state;
-    const monthsCommit = formData.monthsCommit || 0
-    const monthsPrepay = formData.monthsPrepay || 0
 
     return (
-      <Box p={3}>
+      <Box p={3} mx='auto' style={{maxWidth: 960}}>
         <Form
+          formData={formData}
           schema={schema.json_schema}
           uiSchema={schema.ui_object}
-          onFormChange={this.onChange}
+          onChange={this.onChange}
         />
-
-        <Box
-          mt={3}
-        >
-          {monthsCommit * monthsPrepay}
-        </Box>
       </Box>
     );
   }
